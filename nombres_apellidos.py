0b8fdb71aff46c10280b0946384e5d79ca4209f6
RenzoHuaman <re.huaman@duocuc.cl>
2024-07-11 21:44:25 +0000

    Script que muestra una lista de los intregrantes

---
+++ nombres_apellidos.py
@@ -0,0 +1,12 @@
+# nombres_apellidos.py
+
+# Lista de nombres y apellidos de los integrantes del grupo
+integrantes = [
+    "Renzo Huaman",
+    "Marcelo Calderon",
+    "Francisco Garate"
+]
+
+# Imprimir la lista de nombres y apellidos
+for integrante in integrantes:
+    print(integrante)
